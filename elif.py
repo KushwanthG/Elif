ba4994e9c50f49e64fe3c352dc701389f0f9e6f4
Kushwanth Gondi <36468390+KushwanthG@users.noreply.github.com>
2020-04-09 23:31:55 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,12 @@
+#if True:
+    #print("it's true")
+loc='Game'
+if loc=='mobileshop':
+    print('shop is great!')
+elif loc == 'movie':
+    print('A great movie!')
+elif loc=='store':
+    print("good store!")
+else:
+    print("i don't no much...")
+
